5b70d6a2ccf0785fc98b8460311bf14cad428c2c
Meaghan Jones and Molly McGlone <meaghan+molly@>
2016-08-23 16:31:23 -0700

    style index page

--- app/router.js
+++ app/router.js
@@ -10,6 +10,8 @@ Router.map(function() {
   this.route('legislators-results', {path:'/legislators-results/:zip'});
   this.route('committees');
   this.route('committee', {path:'/committee/:committee_id'});
+  this.route('bills');
+  this.route('bills-results', {path: '/bills-results/:recentBills'});
 });
 
 export default Router;
---
+++ tests/unit/helpers/radio-buttons-test.js
@@ -0,0 +1,10 @@
+import { radioButtons } from 'enlightened-citizenry/helpers/radio-buttons';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | radio buttons');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = radioButtons([42]);
+  assert.ok(result);
+});
